a60353186ce8accce81df52de84f9d544cf5b878
Cristian Pacheco <cgpz2001@gmail.com>
2022-10-12 11:59:24 -0500

    proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Termi from './component/Termi'
+import Term_f from './component/term_f'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Term_f/>
+      
+      <Termi/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Termi.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './termi.css'
+
+
+const Termi = () => {
+  return (
+    <article className='termi_main'>
+         <div className='cuadro'>
+         <p className='tex_1'>
+   ¿Te gustaria colaborar juntos para crear productos increibles?
+</p>
+<div className='ingre'>
+ 
+ <font><input type="ingresar correo"placeholder='Deja tu correo aqui' /></font>
+ <div className='but'><button className='button_1'> 
+  enviar 
+ </button></div>
+ 
+</div>
+ 
+      </div>
+      
+
+    
+     
+    </article>
+)}
+
+export default Termi
